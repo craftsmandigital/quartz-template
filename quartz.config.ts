786e83fbf562bd27b8854d33dd261822f243b6ec
craftsmandigital <htcjon10@gmail.com>
2025-08-06 21:55:40 +0200

    Quartz sync: Aug 6, 2025, 9:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,8 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
+import "dotenv/config" // XXX
+
 /**
  * Quartz 4 Configuration
  *
@@ -8,17 +10,17 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: process.env.SITE_NAME ?? "Default Title", // Use the environment variable XXX
+    pageTitleSuffix: process.env.SITE_NAME ? " 🡆 " + process.env.SITE_NAME : "",  // XXX
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    locale: process.env.LOCALE ?? "en-US", // XXX
+    baseUrl: process.env.BASE_URL ?? "quartz.jzhao.xyz", // Use the environment variable XXX
+    ignorePatterns: ["private", "templates", ".obsidian", "**/.conform.*", "**/*.tmp", "**/.*"],
+    defaultDateType: process.env.DEFAULT_DATE_TYPE ?? "modified", // created, modified, published XXX
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -29,23 +31,23 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#f5f2e9",
+          lightgray: "#e0dcd3",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#384b73",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#1a1915",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#5d7da5",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
@@ -69,7 +71,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: (process.env.CRAWL_LINKS ?? "shortest") }), // shortest, relative, absolute XXX
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
